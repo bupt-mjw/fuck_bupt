14544cc4a2b8719804a411ffd42e2ba841c8eccd
bupt-mjw <69418775+bupt-mjw@users.noreply.github.com>
2022-03-12 10:59:03 +0800

    Add files via upload

--- foo.js
+++ foo.js
@@ -35,55 +35,38 @@ function curDateTime() {
 
     
     
-    // function Enter(e) {
-    //   console.log(e)
-    //   var name = document.getElementById("input-name").value;
-    //   var aca = document.getElementById("input-aca").value;
-    //   var number = document.getElementById("input-number").value;
-
-    //   document.getElementById("content").style.display = "";
-    //   document.getElementById("input-box").style.display = "none";
-
-    //   if (name != '') {
-    //     document.getElementById("info-name").innerHTML = name;
-    //   }
-    //   if (aca != '') {
-    //     document.getElementById("info-aca").innerHTML = aca;
-    //   }
-    //   if (number != '') {
-    //     document.getElementById("info-number").innerHTML = number;
-    //   }
-    //   var status = ['允许入校', '允许出校'];
-    //   document.getElementById("status").innerHTML = status[parseInt(e)];
-    //   document.getElementById("time").innerHTML = curTime;
-    // }
     function Enter(e) {
-      var obj = document.getElementById('ni');
-　　
-      console.log(e);
+      // console.log(e)
+      var name = document.getElementById("input-name").value;
+      // var aca = document.getElementById("input-aca").value;
+      // var number = document.getElementById("input-number").value;
+
       document.getElementById("content").style.display = "";
       document.getElementById("input-box").style.display = "none";
-      document.getElementById("time").innerHTML = curTime;
-      if (parseInt(e) == 0) {
-        obj.src = "./assest/gzd.png"
-        // obj.setAttribute('./assest/gzd.png', './assest/gzd.png');//把图片修改为目标路径
-        // document.getElementById("ni").img.src = './assest/gzd.png'
-      }else if(parseInt(e) == 1) {
-        document.getElementById('info-name').innerHTML = '曾凯林'
-        obj.src = "./assest/zkl.jpg"
-        // document.getElementById("ni").img.src = './assest/1.png'
-      }else if(parseInt(e) == 2) {
-        document.getElementById('info-name').innerHTML = '蔺展琨'
-        obj.src = "./assest/lzk.jpg"
-      }else if(parseInt(e) == 3) {
-        document.getElementById('info-name').innerHTML = '赵石磊'
-        obj.src = "./assest/zsl.jpg"
-      }else if(parseInt(e) == 4) {
-        document.getElementById('info-name').innerHTML = '郗厚印'
-        obj.src = "./assest/xhy.jpg"
-      }else if(parseInt(e) == 5) {
-        document.getElementById('info-name').innerHTML = '乔博'
-        obj.src = "./assest/qy.jpg"
+
+      if (name != '') {
+        document.getElementById("info-name").innerHTML = name;
       }
-      
+      // if (aca != '') {
+      //   document.getElementById("info-aca").innerHTML = aca;
+      // }
+      // if (number != '') {
+      //   document.getElementById("info-number").innerHTML = number;
+      // }
+      var status = ['允许入校', '允许出校'];
+      document.getElementById("status").innerHTML = status[parseInt(e)];
+      document.getElementById("time").innerHTML = curTime;
     }
+    
+function change() {
+  let content = document.getElementById("status").innerHTML;
+  if(content == "允许入校") {
+    document.getElementById("status").innerHTML = "允许出校";
+  }else {
+    document.getElementById("status").innerHTML = "允许入校";
+  }
+  
+  
+}
+
+
